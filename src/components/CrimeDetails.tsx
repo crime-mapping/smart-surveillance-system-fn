d5baf0b99b9b7225925f77395eab1d7eeaefe105
Ishimwe7 <ishimweinstein@gmail.com>
2025-05-05 18:54:51 +0200

    improved crime details by adding supporting image

--- src/components/CrimeDetails.tsx
+++ src/components/CrimeDetails.tsx
@@ -91,7 +91,7 @@ const CrimeDetails: React.FC = () => {
       {loading ? (
         <SingleCrimeSkeleton />
       ) : (
-        <div className="mt-24 p-6 my-4 bg-white shadow rounded-md">
+        <div className="mt-20 p-6 my-4 bg-white shadow rounded-md">
           <span
             onClick={() => {
               navigate("/reports");
@@ -138,9 +138,23 @@ const CrimeDetails: React.FC = () => {
 
           <div className="mb-4">
             <h4 className="font-semibold">Evidences</h4>
-            <div className="flex items-center text-gray-700">
-              <Camera className="w-5 h-5 mr-2" />2 photos taken
-            </div>
+            {crime?.supportingImage ? (
+              <>
+                {" "}
+                <div className="flex items-center text-gray-700">
+                  <Camera className="w-5 h-5 mr-2" />1 photo was taken
+                </div>
+                <img
+                  className="rounded-md h-80"
+                  src={crime?.supportingImage}
+                  alt="Crime Supporting image"
+                />
+              </>
+            ) : (
+              <p className="italic text-md mt-2 text-red-500">
+                No evidences found
+              </p>
+            )}
           </div>
 
           <button
